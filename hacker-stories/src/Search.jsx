60126a7232ab08f5b335fbcc39a5224698fbc15b
ELhabib <elhabibbennani10@gmail.com>
2025-08-31 20:11:34 +0100

    Handler Function in JSX

--- hacker-stories/src/Search.jsx
+++ hacker-stories/src/Search.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Search = () => {
+  const handelChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div>
       <h3>Search</h3>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handelChange} />
     </div>
   );
 };
