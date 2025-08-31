2ef4cbe75bc6991232613b7d735f688509a50985
ELhabib <elhabibbennani10@gmail.com>
2025-08-31 13:40:27 +0100

    component trees

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,12 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
+import List from "./List";
+import Search from "./Search";
 
 const title = "React";
-const welcome = {
-  greeting: "Hey",
-  title: "React",
-};
-
 function getTitle(title) {
   return title;
 }
@@ -18,25 +15,6 @@ const mapped = array.map((x) => x * 2);
 
 console.log(mapped);
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const articles = [
   { id: 1, title: "elhabib benani" },
   { id: 2, title: "omaar bennani" },
@@ -46,29 +24,11 @@ const articles = [
 function App() {
   return (
     <div>
-      <h1>{title} helloo would</h1>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
-
       <h1>Hello {getTitle("React")}</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-
+      <Search />
       <hr />
-      <ul>
-        {list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        ))}
-      </ul>
+      <List />
 
       <h2>List friends</h2>
       <ul>
---
+++ hacker-stories/src/Search.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <h3>Search</h3>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+};
+
+export default Search;
